b04f0b93d92de228f312e837215acb0aea0ee992
jclos305 <jclos305@hotmail.com>
2022-03-15 11:45:17 -0400

    added username, password, and app init

--- server.js
+++ server.js
@@ -8,5 +8,22 @@ const connection = mysql.createConnection({
     host: 'localhost',
 
 
-// Local port if not 3305
+// Local port (or automated to 3305)
   port: 3305,
+
+   //  username
+   user: 'root',
+
+   //  password
+   password: 'NewPassWordPLZ',
+   database: 'employeesDB'
+});
+
+connection.connect(function (err) {
+   if (err) throw err;
+   console.log("connected as id " + connection.threadId);
+   console.log(`Let's a-go!`)
+
+    // initializes application
+    firstPrompt();
+});
